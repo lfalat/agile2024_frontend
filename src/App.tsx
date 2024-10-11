ef4e245f526642aad34de6839c51dc4b18107a43
Patrik Balvan <ba.patko@gmail.com>
2024-10-11 17:03:40 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import axios from 'axios';
 
 const App: React.FC = () => {
+  const [value, setValue] = React.useState<string>("zatial nic");
+
+  const getData = () => {
+    axios.get('/helloworld')
+      .then((res) => {
+        setValue(res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
     <>
-      hello world
+      {value}
+      <Button variant='contained' onClick={() => {getData()}}>Get data</Button>
     </>
   )
 }
